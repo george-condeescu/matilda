0bab0e951983caa7274afc8f40663d97aee06d65
george-condeescu <george_condeescu@yahoo.com>
2024-12-16 14:23:41 +0200

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Matilda</a>
+      </header>
+      <main>list products here</main>
+    </div>
+  );
+}
+
+export default App;
